b6f59d40c72fca3b6c9b4b8fb096cf7a856691ff
zogich <tkachenko.ei@students.dvfu.ru>
2023-11-25 15:55:50 +1000

    add login and password validation

--- frontend_vite/src/pages/RegisterPage.tsx
+++ frontend_vite/src/pages/RegisterPage.tsx
@@ -11,6 +11,12 @@ function RegisterPage(){
     const [password, setPassword] = useState('');
     const navigation = useNavigate();
 
+    const [passwordErrorMessage, setPasswordMessage] = useState(<></>)
+    const [passwordLengthMessage, setPasswordLenghtMessage] = useState(<></>)
+
+    const [loginErrorMessage, setLoginMessage] = useState(<></>)
+    const [loginLengthMessage, setLoginLenghtMessage] = useState(<></>)
+
     function handleChangeLogin(e){
         setLogin(e.target.value)
     }
@@ -18,38 +24,82 @@ function RegisterPage(){
         setPassword(e.target.value)
     }
 
-    async function signUP(){
-        await api.post('/auth/signup', {username: login, password: password}).then(response => {
-            console.log(response)
-        })
-        .catch(error=>{
-            console.log(error)
-        })
+    function validateLogin(){
+        const reg = new RegExp('^[A-Za-z][A-Za-z\\d]+$')
+        let validate = true;
+        setLoginMessage(<></>);
+        setLoginLenghtMessage(<></>);
+        if (!reg.test(login)){
+            setLoginMessage(<div>Логин должен состоять из латинских букв и цифр и начинаться с буквы.</div>);
+            validate = false;
+        }
+        if ( login.length < 6 || login.length > 20 ){
+            setLoginLenghtMessage(<div>Логин должен быть длиной от 6 до 20 символов</div>);
+            validate = false;
+        }
+        return validate
+    }
 
-        await api.post('/auth/login', {username: login, password: password}).then(
-            response => {
-                localStorage.setItem('access', response.data.access);
-                localStorage.setItem('refresh', response.data.refresh);
-            }
-        ).catch(
-            error => {
+    function validatePassword(){
+        const reg = new RegExp('^[A-Za-z][A-Za-z\\d]+$');
+        let validate = true;
+        setPasswordMessage(<></>);
+        setPasswordLenghtMessage(<></>)
+        if (!reg.test(password)){
+            setPasswordMessage(<div>Пароль должен состоять из латинских букв и цифр и начинаться с буквы.</div>);
+            validate = false;
+        }
+        if ( password.length < 6 || password.length > 20){
+            setPasswordLenghtMessage(<div>Пароль должен быть длиной от 6 до 20 символов</div>)
+            validate = false;
+        }
+        return validate;
+    }
+
+
+    async function signUP(){
+        const isLoginValid = validateLogin();
+        const isPasswordValid = validatePassword()
+        if ( isLoginValid && isPasswordValid ) {
+
+
+            await api.post('/auth/signup', { username: login, password: password }).then(response => {
+                console.log(response)
+            })
+                .catch(error => {
+                    console.log(error)
+                })
+
+            await api.post('/auth/login', { username: login, password: password }).then(
+                response => {
+                    localStorage.setItem('access', response.data.access);
+                    localStorage.setItem('refresh', response.data.refresh);
+                }
+            ).catch(
+                error => {
+                    console.log(error)
+                    return;
+                }
+            )
+            await api.get('/auth/profile').then(
+                response => {
+                    acceptAuthentication(response.data)
+                }).catch(error => {
                 console.log(error)
-                return;
-            }
-        )
-        await api.get('/auth/profile').then(
-            response =>{
-                acceptAuthentication(response.data)
-            }).catch(error => {
-            console.log(error)
-        })
-        navigation('/')
+            })
+            navigation('/')
+        }
+
     }
 
     return <>
         <div className={styles['register-page']}>
         <input type={"text"} value={login} onChange={handleChangeLogin} placeholder={'Введите логин'}/>
+            {loginErrorMessage}
+            {loginLengthMessage}
         <input type={"password"} value={password} onChange={handleChangePassword} placeholder={'Введите пароль'}/>
+            {passwordErrorMessage}
+            {passwordLengthMessage}
         <button onClick={signUP}>Зарегистрироваться</button>
             <Link to={'/'}>Назад</Link>
         </div>
